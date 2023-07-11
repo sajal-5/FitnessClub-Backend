245aa0b46d5a5c030a55b8a0a4c7c4572f938e2a
sajal-555 <sajal.verma2020@vitstudent.ac.in>
2023-07-11 12:05:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+
+dotenv.config({ path: './config.env' })
+
+//database connection
+require('./db/conn');
+// const Member=require('./model/memberSchema');
+
+app.use(express.json());
+
+//linking router files
+app.use(require('./router/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+
+
+// app.get('/', (req, res) => {
+//     res.send('hello world');
+// });
+
+// app.get('/about', (req, res) => {
+//     res.send('about');
+// });
+
+// app.get('/contact', (req, res) => {
+//     res.send('contactss');
+// });
+
+// app.get('/trainers', (req, res) => {
+//     res.send('trainers');
+// });
+
+// app.get('/classes', (req, res) => {
+//     res.send('classes');
+// });
+
+// app.get('/signin', (req, res) => {
+//     res.send('signin');
+// });
+
+// app.get('/signup', (req, res) => {
+//     res.send('signup');
+// });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
