4ff6253f2867f995f69e16ed013fa49875c968e6
sajal-5 <100183942+sajal-5@users.noreply.github.com>
2023-07-11 21:13:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 
-// const cors=require('cors');
+const cors=require('cors');
 
 dotenv.config({ path: './config.env' })
 
@@ -12,44 +12,14 @@ require('./db/conn');
 
 app.use(express.json());
 
-// app.use(cors({
-//     origin:'*'
-// }))
+app.use(cors({
+    origin:'*'
+}))
 
 //linking router files
 app.use(require('./router/auth'));
 
 const PORT = process.env.PORT || 5000;
-
-
-
-// app.get('/', (req, res) => {
-//     res.send('hello world');
-// });
-
-// app.get('/about', (req, res) => {
-//     res.send('about');
-// });
-
-// app.get('/contact', (req, res) => {
-//     res.send('contactss');
-// });
-
-// app.get('/trainers', (req, res) => {
-//     res.send('trainers');
-// });
-
-// app.get('/classes', (req, res) => {
-//     res.send('classes');
-// });
-
-// app.get('/signin', (req, res) => {
-//     res.send('signin');
-// });
-
-// app.get('/signup', (req, res) => {
-//     res.send('signup');
-// });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
